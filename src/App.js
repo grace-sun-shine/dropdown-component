168dfa465a008cd8ed84449be7bc8e81c82af914
Taige (Grace) Sun <taiges@andrew.cmu.edu>
2023-03-21 15:17:46 -0700

    basic dropdown before style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import Dropdown from './components/Dropdown';
 
 function App() {
+  const options = ['Oliver Hansen', 'Van Henry', 'April Tucker', 'Ralph Hubbard'];
+
   return (
-    <div>
-      <Dropdown />
+    <div className='App'>
+      <h1>Hive Frontend Engineer Challenge</h1>
+      <div className="display">
+        <h2>Multiple Selection Dropdown</h2>
+        <Dropdown options={options} multiple={true} onSelect={(selected) => console.log(selected)}/>
+        <h2>Single Selection Dropdown</h2>
+        <Dropdown options={options} multiple={false} onSelect={(selected) => console.log(selected)}/>
+      </div>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import './Dropdown.css';
 
+// create a reusable dropdown component
+const Dropdown = ({ options, multiple, onSelect}) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [selectedOptions, setSelectedOptions] = React.useState([]);
 
-const Dropdown = ({ }) => {
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    const handleOptionClick = (option) => {
+        if (!multiple) {
+            setSelectedOptions([option]);
+            onSelect(option);
+            toggleDropdown();
+        }else{
+            let newSelectedOptions;
+            if (selectedOptions.includes(option)) {
+                newSelectedOptions = selectedOptions.filter((item) => item !== option)
+                setSelectedOptions(newSelectedOptions);
+                onSelect(newSelectedOptions);
+            } else {
+                newSelectedOptions = [...selectedOptions, option];
+                setSelectedOptions(newSelectedOptions);
+                onSelect(newSelectedOptions);
+            }
+
+        }
+    }
+
     return (
-        <div> test </div>
+        <div className='dropdown'> 
+            <div className='dropdown-header' onClick={toggleDropdown}>
+            {selectedOptions.length > 0 ? selectedOptions.join(', ') : 'Select option(s)'}
+            <span className={`dropdown-arrow ${isOpen ? 'open' : ''}`}>&#9660;</span>
+            </div>
+            {isOpen && (
+                <div className='dropdown-body'>
+                    {options.map((option, index) => (
+                        <div
+                            key={index}
+                            className={`dropdown-option ${selectedOptions.includes(option) ? 'selected' : ''}`}
+                            onClick={() => handleOptionClick(option)}
+                        >
+                            {option}
+                        </div>
+
+                    ))}
+                </div>
+            )}
+        </div>
     );
 }
 
